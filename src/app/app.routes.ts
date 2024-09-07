44bf3b613a4ed015ca23a8283fcd8d5dcb7b03e0
MateoSpatola <mateospatola@hotmail.com>
2024-09-07 14:10:42 -0300

    Ruteo e instalacion de angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/main/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', loadComponent: () => import('./pages/auth/login/login.component').then((m) => m.LoginComponent) },
+    { path: 'signup', loadComponent: () => import('./pages/auth/sign-up/sign-up.component').then((m) => m.SignUpComponent) },
+    { path: 'quiensoy', loadComponent: () => import('./pages/main/quien-soy/quien-soy.component').then((m) => m.QuienSoyComponent) },
+    { path: '**', loadComponent: () => import('./pages/main/error/error.component').then((m) => m.ErrorComponent) }
+];
