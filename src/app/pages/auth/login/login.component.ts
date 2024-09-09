2cf09c20885e31159faa79cbc07480bc8253aab4
MateoSpatola <mateospatola@hotmail.com>
2024-09-09 00:45:44 -0300

    Creacion del login.

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  @Input() passwordType: string = "password";
+  hide: boolean = true;
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  showOrHidePassword() : void {
+    this.hide = !this.hide;
+    this.passwordType = this.hide ? 'password' : 'text';
+  }
 }
+
