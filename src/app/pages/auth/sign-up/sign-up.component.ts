d75ab3e8fa98942668ae222cc5ca985216643de6
MateoSpatola <mateospatola@hotmail.com>
2024-09-09 09:31:07 -0300

    Creacion del signup.

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+
+  @Input() passwordType: string = "password";
+  hide: boolean = true;
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)])
+
+  })
+
+  showOrHidePassword() : void {
+    this.hide = !this.hide;
+    this.passwordType = this.hide ? 'password' : 'text';
+  }
 
 }
