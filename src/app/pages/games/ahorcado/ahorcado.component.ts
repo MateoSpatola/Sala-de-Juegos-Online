2430a4e40d24d06e1a8bf014ddc87f078a140d6c
MateoSpatola <mateospatola@hotmail.com>
2024-10-01 19:02:48 -0300

    Creacion juego ahorcado.

--- src/app/pages/games/ahorcado/ahorcado.component.ts
+++ src/app/pages/games/ahorcado/ahorcado.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ChatComponent } from '../../../shared/chat/chat.component';
+import { HttpClient } from '@angular/common/http';
+import { NotificationService } from '../../../services/notification.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-ahorcado',
@@ -16,4 +19,72 @@ import { ChatComponent } from '../../../shared/chat/chat.component';
 })
 export class AhorcadoComponent {
 
+  http = inject(HttpClient);
+  private _notificationService= inject(NotificationService);
+
+  apiUrl: string = "https://clientes.api.greenborn.com.ar/public-random-word"
+
+  message: string = "";
+  life: number = 5;
+  loading: boolean = false;
+  word: string = "";
+  gameOver: boolean = false;
+  keysPressed: string[] = [];
+
+  keyboard: string[][] = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+  ];
+
+  ngOnInit() {
+  }
+
+  async getRandomWord() {
+    try {
+      this.loading = true;
+      const observable = this.http.get(this.apiUrl);
+      const response: any = await firstValueFrom(observable);
+      this.word = this.convertWord(response[0]);
+    } 
+    catch (error: any) {
+      this._notificationService.showAlert('Error inesperado: ' + error.message, 'error', 2000);
+    }
+    finally {
+      this.loading = false;
+    }
+  }
+
+  convertWord(word: string): string {
+    return word.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
+  }
+
+  handleKeyPress(key: string) {
+    console.log('Tecla presionada:', key);
+    if (!this.word.includes(key)) {
+      this.life--;
+    }
+    this.keysPressed.push(key);
+    this.checkWinner();
+  }
+
+  checkWinner() {
+    if (this.life == 0) {
+      this.message = "¡PERDISTE! La palabra era: " + this.word;
+      this.gameOver = true;
+    }
+    else if(this.word.split('').every(key => this.keysPressed.includes(key)))
+    {
+      this.message = "¡GANASTE!";
+      this.gameOver = true;
+    }
+  }
+
+  resetGame() {
+    this.message = "";
+    this.life = 5;
+    this.word = "";
+    this.gameOver = false;
+    this.keysPressed = [];
+  }
 }
